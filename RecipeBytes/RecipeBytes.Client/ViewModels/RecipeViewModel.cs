31b39b1b1be135ddebcbcb1ceeac8116d9ac9ec1
Allan Clark <kqspadesisthenuts@gmail.com>
2024-04-12 16:39:07 -0400

    Edit Recipe validation

--- RecipeBytes/RecipeBytes.Client/ViewModels/RecipeViewModel.cs
+++ RecipeBytes/RecipeBytes.Client/ViewModels/RecipeViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace RecipeBytes.Client.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBytes.Client.ViewModels
 {
     public record RecipeViewModel
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Please enter a recipe name.")]
         public string Name { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a description.")]
         public string Description { get; set; } = string.Empty;
         public List<TagViewModel>? Tags { get;}
         public List<string>? SelectedIngredients { get; set; }
