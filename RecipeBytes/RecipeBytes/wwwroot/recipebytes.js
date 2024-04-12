1cbb70a213ac6f826221d4b498d74b05bc7995a9
pieAre5quare <allan.clark00@gmail.com>
2024-04-12 14:58:51 -0400

    fix selector for JQuery filter

--- RecipeBytes/RecipeBytes/wwwroot/recipebytes.js
+++ RecipeBytes/RecipeBytes/wwwroot/recipebytes.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
-  // Toggle Bootstrap theme attr
-  $('body').on('click','.theme-toggler', function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+    // Toggle Bootstrap theme attr
+    $('body').on('click', '.theme-toggler', function (event) {
+        event.preventDefault();
 
-    $(this).toggleClass("btn-outline-dark btn-outline-light");
+        $(this).toggleClass("btn-outline-dark btn-outline-light");
 
-    if ($("html").attr("data-bs-theme") == "light") {
-      $("html").attr("data-bs-theme","dark");
-      $(this).html('☾ Dark Mode');
-    } else {
-      $("html").attr("data-bs-theme","light");
-      $(this).html('☀︎ Light Mode');
-    }
-  });
+        if ($("html").attr("data-bs-theme") == "light") {
+            $("html").attr("data-bs-theme", "dark");
+            $(this).html('☾ Dark Mode');
+        } else {
+            $("html").attr("data-bs-theme", "light");
+            $(this).html('☀︎ Light Mode');
+        };
+    });
 
-    $("#filterInput").on("keyup", function () {
+    $('body').on('keyup', '#filter-input', function (event) {
         var value = $(this).val().toLowerCase();
-        $("#recipeTable tr").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
+        $("#recipe-list > div").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
     });
 });
